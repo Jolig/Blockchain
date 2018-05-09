fb715066789525ed9cc05fb10db22abe7f8eb62a
Akhila Veerapuraju <dhanya.akhila@iiitb.org>
2018-05-09 10:33:49 +0530

    changed go code

--- crowd_fund_chaincode.go
+++ crowd_fund_chaincode.go
@@ -25,47 +25,47 @@ type user struct{
 func (t *CrowdFundChaincode) Init(stub shim.ChaincodeStubInterface,function string, args []string) ([]byte, error) {
 
     fmt.Printf("Hello ")
-	var err error
+    var err error
 
-	if len(args) != 2 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 2")
-	}
+    if len(args) != 2 {
+        return nil, errors.New("Incorrect number of arguments. Expecting 2")
+    }
 
-	if err != nil {
-		return nil, err
-	}
+    if err != nil {
+        return nil, err
+    }
 
     record := UserLog{}
     record.Email = "NotApplicable"
     record.Data = []user{};
-	newrecordByte, err := json.Marshal(record);
+    newrecordByte, err := json.Marshal(record);
     if err != nil {
-		return nil, err
+        return nil, err
     }
 
-	err = stub.PutState("default",newrecordByte);
-	if err != nil {
-		return nil, err
-	}
+    err = stub.PutState("default",newrecordByte);
+    if err != nil {
+        return nil, err
+    }
 
     return nil, nil
 
 }
 
 func (t *CrowdFundChaincode) Invoke(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-
-	var account string
+    var account string
     var err error
 
     if len(args) != 5 {
         return nil, errors.New("Incorrect number of arguments. Expecting 4  .")
     }
-	account = args[0]
+
+    account = args[0]
 
     recordByte, err := stub.GetState(account);
     fmt.Println(recordByte);
-	if err != nil {
-		return nil, err
+    if err != nil {
+        return nil, err
     }
 
     record := UserLog{}
@@ -75,50 +75,51 @@ func (t *CrowdFundChaincode) Invoke(stub shim.ChaincodeStubInterface, function s
     temp.Timestamp = args[1]
     temp.Role = args[2]
     temp.Action =  args[3]
-	temp.WorldID = args[4]
+    temp.WorldID = args[4]
 
-	var artist UserLog
-	if recordByte != nil {
-	    err_ret := json.Unmarshal(recordByte,&artist);
-		if(err_ret == nil) {
-			record.Data = artist.Data;
-		}
-	}
-
-	record.Data = append(record.Data, temp)
-	newrecordByte, err := json.Marshal(record);
-	if err != nil {
-		return nil, err
+    var artist UserLog
+    if recordByte != nil {
+        err_ret := json.Unmarshal(recordByte,&artist);
+        if(err_ret == nil) {
+            record.Data = artist.Data;
+        }
     }
 
-	err = stub.PutState(account,newrecordByte);
+    record.Data = append(record.Data, temp)
+
+    newrecordByte, err := json.Marshal(record);
     if err != nil {
-		return nil, err
+        return nil, err
     }
 
-	return nil, nil
+    err = stub.PutState(account,newrecordByte);
+    if err != nil {
+        return nil, err
+    }
+
+    return nil, nil
 
 }
 
 func (t *CrowdFundChaincode) Query(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
 
-	if function != "read" {
+    if function != "read" {
         return nil, errors.New("Invalid query function name. Expecting \"query\".")
     }
 
-	var err error
+    var err error
 
-	if len(args) != 1 {
+    if len(args) != 1 {
         return nil, errors.New("Incorrect number of arguments. Expecting name of the state variable to query.")
     }
 
-	var   account = args[0]
-	accountValueBytes ,err := stub.GetState(account)
-	if err != nil {
-		return nil, err
+    var   account = args[0]
+    accountValueBytes ,err := stub.GetState(account)
+    if err != nil {
+        return nil, err
     }
 
-	return accountValueBytes, nil
+    return accountValueBytes, nil
 
 }
 
@@ -128,5 +129,4 @@ func main() {
     if err != nil {
         fmt.Printf("Error starting CrowdFundChaincode: %s", err)
     }
-
 }
